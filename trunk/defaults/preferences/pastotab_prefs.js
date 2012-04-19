189333124315720c1d6e20eeeb0eed3efcd45225
zoolcar9@gmail.com <zoolcar9@gmail.com@05e6302d-184c-0410-a112-c17eeddeda9d>
2012-04-19 20:17:46 +0000

    + Load donation page at AMO on first installation only x Refactored

--- trunk/defaults/preferences/pastotab_prefs.js
+++ trunk/defaults/preferences/pastotab_prefs.js
@@ -1,2 +1,8 @@
 pref("extensions.pastetotab@loucypher.description", "chrome://pastetotab/locale/pasteToTab.properties");
 pref("extensions.pastetotab.debug", false);
+pref("extensions.pastetotab.firstRun", true);
+pref("extensions.pastetotab.contributionURL", "https://addons.mozilla.org/%LOCALE%/%APP%/addon/paste-to-tab-and-go/contribute/installed/");
+
+// JS functions for keyconfig extension
+pref("keyconfig.main.xxx_key1_Paste to New Tab & Go", "!][][][// Added by Paste to Tab and Go extension\n// https://addons.mozilla.org/addon/paste-to-tab-and-go/about\n\nPasteToTab.go();\n][chrome://browser/content/browser.xul");
+pref("keyconfig.main.xxx_key1_Paste to New Tab & Search", "!][][][// Added by Paste to Tab and Go extension\n// https://addons.mozilla.org/addon/paste-to-tab-and-go/about\n\nPasteToTab.search();\n][chrome://browser/content/browser.xul");
