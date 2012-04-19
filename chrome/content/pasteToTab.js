ea1bb582c3722b58f1cc78073be93d757d35dc59
zoolcar9 <zoolcar9@gmail.com>
2012-04-19 14:00:16 +0000

    x Bumped version number + Add pasted string to textbox autocomplete x Refactored

--- chrome/content/pasteToTab.js
+++ chrome/content/pasteToTab.js
@@ -1,36 +1,37 @@
 /*
-  Version: MPL 1.1/GPL 2.0/LGPL 2.1
+    Version: MPL 1.1/GPL 2.0/LGPL 2.1
 
-  The contents of this file are subject to the Mozilla Public License Version
-  1.1 (the "License"); you may not use this file except in compliance with
-  the License. You may obtain a copy of the License at
-  http://www.mozilla.org/MPL/1.1/
+    The contents of this file are subject to the Mozilla Public License
+    Version 1.1 (the "License"); you may not use this file except in
+    compliance with the License. You may obtain a copy of the License
+    at http://www.mozilla.org/MPL/1.1/
 
-  Software distributed under the License is distributed on an "AS IS" basis,
-  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
-  for the specific language governing rights and limitations under the
-  License.
+    Software distributed under the License is distributed on an
+    "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+    See the License for the specific language governing rights and
+    limitations under the License.
 
-  The Original Code is "Paste to Tab and Go" extension
+    The Original Code is "Paste to Tab and Go" extension
 
-  The Initial Developer of the Original Code is LouCypher.
-  Portions created by the Initial Developer are Copyright (C) 2006
-  the Initial Developer. All Rights Reserved.
+    The Initial Developer of the Original Code is LouCypher.
+    Portions created by the Initial Developer are Copyright (C) 2006
+    the Initial Developer. All Rights Reserved.
 
-  Contributor(s):
-    LouCypher <loucypher@mozillaca.com>
+    Contributor(s):
+      LouCypher <loucypher@mozillaca.com>
 
-  Alternatively, the contents of this file may be used under the terms of
-  either the GNU General Public License Version 2 or later (the "GPL"), or
-  the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
-  in which case the provisions of the GPL or the LGPL are applicable instead
-  of those above. If you wish to allow use of your version of this file only
-  under the terms of either the GPL or the LGPL, and not to allow others to
-  use your version of this file under the terms of the MPL, indicate your
-  decision by deleting the provisions above and replace them with the notice
-  and other provisions required by the GPL or the LGPL. If you do not delete
-  the provisions above, a recipient may use your version of this file under
-  the terms of any one of the MPL, the GPL or the LGPL.
+    Alternatively, the contents of this file may be used under the terms
+    of either the GNU General Public License Version 2 or later (the "GPL"),
+    or the GNU Lesser General Public License Version 2.1 or later
+    (the "LGPL"), in which case the provisions of the GPL or the LGPL are
+    applicable instead of those above. If you wish to allow use of your
+    version of this file only under the terms of either the GPL or the LGPL,
+    and not to allow others to use your version of this file under the terms
+    of the MPL, indicate your decision by deleting the provisions above and
+    replace them with the notice and other provisions required by the GPL or
+    the LGPL. If you do not delete the provisions above, a recipient may use
+    your version of this file under the terms of any one of the MPL, the GPL
+    or the LGPL.
 */
 
 var PasteToTab = {
@@ -113,8 +114,10 @@ var PasteToTab = {
       var mi = tmpl.cloneNode(true); mi.id = id; mi.removeAttribute("hidden");
       pi.setAttribute("onmouseover", "PasteToTab.updateText(this);");
       pi.setAttribute("onmouseout", "PasteToTab.updateText(this, '');");
-      pi.previousSibling.setAttribute("onmouseover", "PasteToTab.updateText(this);");
-      pi.previousSibling.setAttribute("onmouseout", "PasteToTab.updateText(this, '');");
+      pi.previousSibling.setAttribute("onmouseover",
+                                      "PasteToTab.updateText(this);");
+      pi.previousSibling.setAttribute("onmouseout",
+                                      "PasteToTab.updateText(this, '');");
       var context = pi.parentNode;
       context.insertBefore(mi, pi.nextSibling);
       context.addEventListener("popupshowing", ptt_initURLBar = function(e) {
@@ -122,9 +125,9 @@ var PasteToTab = {
       }, false);
       context.removeEventListener("popuphiding", ptt_initURLBar, false);
     }
-    this.debug("Menuitem has " +
-               (document.getElementById(id) ? "" : "NOT ") +
-               "been inserted into URL Bar context menu.");
+    this.debug("Menuitem has "
+             + (document.getElementById(id) ? "" : "NOT ")
+             + "been inserted into URL Bar context menu.");
   },
 
   // Insert 'Paste to New Tab & Search' menuitem into Search Bar context menu
@@ -133,12 +136,15 @@ var PasteToTab = {
     var pi = this.pasteAndSearch;
     this.debug("Paste & Search = " + pi);
     if (!document.getElementById(id) && pi) {
-      var tmpl = document.getElementById("template-paste-to-new-tab-and-search");
+      var tmpl = document.getElementById("template-paste-to-new-tab-"
+                                       + "and-search");
       var mi = tmpl.cloneNode(true); mi.id = id; mi.removeAttribute("hidden");
       pi.setAttribute("onmouseover", "PasteToTab.updateText(this);");
       pi.setAttribute("onmouseout", "PasteToTab.updateText(this, '');");
-      pi.previousSibling.setAttribute("onmouseover", "PasteToTab.updateText(this);");
-      pi.previousSibling.setAttribute("onmouseout", "PasteToTab.updateText(this, '');");
+      pi.previousSibling.setAttribute("onmouseover",
+                                      "PasteToTab.updateText(this);");
+      pi.previousSibling.setAttribute("onmouseout",
+                                      "PasteToTab.updateText(this, '');");
       var context = pi.parentNode;
       context.insertBefore(mi, pi.nextSibling);
       context.addEventListener("popupshowing",
@@ -147,9 +153,9 @@ var PasteToTab = {
       }, false);
       context.removeEventListener("popuphiding", ptt_initSearchBar, false);
     }
-    this.debug("Menuitem has " +
-               (document.getElementById(id) ? "" : "NOT ") +
-               "been inserted into Search Bar context menu.");
+    this.debug("Menuitem has "
+             + (document.getElementById(id) ? "" : "NOT ")
+             + "been inserted into Search Bar context menu.");
   },
 
   // Load URL or search the web for text into a new tab
@@ -165,7 +171,19 @@ var PasteToTab = {
     var string = aString ? aString
                          : readFromClipboard() ? readFromClipboard()
                                                : "";
-    if (BrowserSearch.searchBar) BrowserSearch.searchBar.value = string;
+    var searchBar = BrowserSearch.searchBar;
+    if (searchBar) {
+      var textBox = searchBar._textbox
+      textBox.value = string;
+      try { // Add pasted string to textbox autocomplete
+        textBox._formHistSvc.addEntry(textBox.
+                                      getAttribute("autocompletesearchparam"),
+                                      string);
+      } catch (ex) {
+        Cu.reportError("Saving search to form history failed: " + ex);
+      }
+    }
+    // Syntax: BrowserSearch.loadSearch(searchString, useNewTab);
     BrowserSearch.loadSearch(string, true);
   },
 
@@ -200,7 +218,7 @@ var PasteToTab = {
     }
   },
 
-  // Display clipboard text on statusbar or tooltip when hover on menuitem
+  // Display clipboard text on tooltip when hover on menuitem
   updateText: function pasteToTab_updateText(aNode, aString) {
     var text = "";
     if (aString) text = aString;
