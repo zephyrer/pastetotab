c5ef1fcf40b44c1012a2cbbeaee6a55ab90fa482
zoolcar9@gmail.com <zoolcar9@gmail.com@05e6302d-184c-0410-a112-c17eeddeda9d>
2012-04-21 21:16:34 +0000

    x Separated prefs

---
+++ trunk/defaults/preferences/keyconfig_prefs.js
@@ -0,0 +1,3 @@
+//JS functions for keyconfig extension
+pref("keyconfig.main.xxx_key1_Paste to New Tab & Go", "!][][][// Added by Paste to Tab and Go extension\n// https://addons.mozilla.org/addon/paste-to-tab-and-go/about\n\nPasteToTab.go();\n][chrome://browser/content/browser.xul");
+pref("keyconfig.main.xxx_key1_Paste to New Tab & Search", "!][][][// Added by Paste to Tab and Go extension\n// https://addons.mozilla.org/addon/paste-to-tab-and-go/about\n\nPasteToTab.search();\n][chrome://browser/content/browser.xul");
