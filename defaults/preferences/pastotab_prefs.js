48aae64496eca2592152a2357cd0316b5112d2ea
zoolcar9 <zoolcar9@gmail.com>
2012-04-23 13:49:10 +0000

    Version 0.6 + Added 'Paste to New Tab & Search' into Search Bar context menu to search in a new tab. x Using tooltip for displaying clipboard text instead of statusbar. + Added Serbian (sr) locale. + Added functions for keyconfig users.

--- defaults/preferences/pastotab_prefs.js
+++ defaults/preferences/pastotab_prefs.js
@@ -2,4 +2,4 @@ pref("extensions.pastetotab@loucypher.description", "chrome://pastetotab/locale/
 pref("extensions.pastetotab.debug", false);
 pref("extensions.pastetotab.firstRun", true);
 pref("extensions.pastetotab.contributionURL", "https://addons.mozilla.org/%LOCALE%/%APP%/addon/paste-to-tab-and-go/contribute/installed/");
-pref("extensions.pastetotab.experimental.pasteTextAndGo", false);
+
