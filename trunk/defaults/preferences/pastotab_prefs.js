045461fa79048770f01276adb94b5f40b8dc8929
zoolcar9@gmail.com <zoolcar9@gmail.com@05e6302d-184c-0410-a112-c17eeddeda9d>
2012-04-30 23:53:31 +0000

    + Added 'Switch to new tab' preference. + Updated sr locale

--- trunk/defaults/preferences/pastotab_prefs.js
+++ trunk/defaults/preferences/pastotab_prefs.js
@@ -1,5 +1,6 @@
 pref("extensions.pastetotab@loucypher.description", "chrome://pastetotab/locale/pasteToTab.properties");
 pref("extensions.pastetotab.debug", false);
+pref("extensions.pastetotab.focusTab", 2);
 pref("extensions.pastetotab.firstRun", true);
 pref("extensions.pastetotab.contributionURL", "https://addons.mozilla.org/%LOCALE%/%APP%/addon/paste-to-tab-and-go/contribute/installed/");
 pref("extensions.pastetotab.urlbar.pasteToNewTabAndGo", true);
