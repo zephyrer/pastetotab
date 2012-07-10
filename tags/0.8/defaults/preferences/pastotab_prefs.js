7b5b0834564b47592fcbd46126b18159b91336db
zoolcar9@gmail.com <zoolcar9@gmail.com@05e6302d-184c-0410-a112-c17eeddeda9d>
2012-07-10 20:03:58 +0000

    0.8

---
+++ tags/0.8/defaults/preferences/pastotab_prefs.js
@@ -0,0 +1,10 @@
+pref("extensions.pastetotab@loucypher.description", "chrome://pastetotab/locale/pasteToTab.properties");
+pref("extensions.pastetotab.debug", false);
+pref("extensions.pastetotab.focusTab", 2);
+pref("extensions.pastetotab.firstRun", true);
+pref("extensions.pastetotab.contributionURL", "https://addons.mozilla.org/%LOCALE%/%APP%/addon/paste-to-tab-and-go/contribute/installed/");
+pref("extensions.pastetotab.urlbar.pasteToNewTabAndGo", true);
+pref("extensions.pastetotab.urlbar.pasteTextAndGo", false);
+pref("extensions.pastetotab.tab.pasteToThisTabAndGo", true);
+pref("extensions.pastetotab.tabbar.pasteToNewTabAndGo", true);
+pref("extensions.pastetotab.searchbar.pasteToNewTabAndSearch", true);
